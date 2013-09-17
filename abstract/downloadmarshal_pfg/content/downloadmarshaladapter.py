72c7657e14c884a64d3f1d9f8058a6aeb74bd8e0
Simone Orsi <simahawk@gmail.com>
2013-09-17 18:37:18 +0200

    default resource from form folder parent

--- abstract/downloadmarshal_pfg/content/downloadmarshaladapter.py
+++ abstract/downloadmarshal_pfg/content/downloadmarshaladapter.py
@@ -1,6 +1,8 @@
 """Definition of the DownloadMarshalAdapter content type
 """
 
+from Acquisition import aq_inner
+from Acquisition import aq_parent
 from AccessControl import ClassSecurityInfo
 
 from zope.interface import implements
@@ -9,6 +11,7 @@ from zope.component import getMultiAdapter
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 # -*- Message Factory Imported Here -*-
 
@@ -30,10 +33,10 @@ DownloadMarshalAdapterSchema = FormAdapterSchema.copy() + atapi.Schema((
         'resource_path',
         widget=atapi.StringWidget(
             label=_(u'path to download resource'),
-            # description=_(u''),
+            description=_(u'if none the container of the form folder will be used as resource'),
         ),
         searchable=0,
-        required=1,
+        required=0,
     ),
 
 ))
@@ -56,19 +59,35 @@ class DownloadMarshalAdapter(FormActionAdapter):
 
     title = atapi.ATFieldProperty('title')
     description = atapi.ATFieldProperty('description')
-
     # -*- Your ATSchema to Python Property Bridges Here ... -*-
+
     security = ClassSecurityInfo()
 
     security.declarePrivate('onSuccess')
     def onSuccess(self, fields, REQUEST=None):
         request = REQUEST or self.REQUEST
-
-        resource_path = self.getResource_path()
-        resource = self.restrictedTraverse(resource_path)
+        resource = self._get_resource()
         marshal = getMultiAdapter((resource, request), IMarshal)
         url = marshal.generate_token_url(resource)
-        print url
+        request.response.redirect(url)
 
+    def _get_resource(self):
+        resource_path = self.getResource_path()
+        if resource_path:
+            resource = self.restrictedTraverse(resource_path)
+        else:
+            resource = self
+            # get form folder
+            while not resource.meta_type == 'FormFolder':
+                resource = aq_parent(aq_inner(resource))
+                if IPloneSiteRoot.providedBy(resource):
+                    resource = None
+                    break
+            # get form folder parent
+            if resource and resource.meta_type == 'FormFolder':
+                resource = aq_parent(aq_inner(resource))
+        if resource is None:
+            raise LookupError("Cannot find any resource!")
+        return resource
 
 atapi.registerType(DownloadMarshalAdapter, PROJECTNAME)
